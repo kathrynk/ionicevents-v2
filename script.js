450f2948a9ad47db662ecabdaacb5932211e85a3
savethebologna <savethebologna@gmail.com>
2014-04-06 19:03:22 -0400

    Optimize a little JS

--- script.js
+++ script.js
@@ -2,14 +2,13 @@
 			width = $(window).width();
 		function fadeheader() {
 			var scrollpos = $(document).scrollTop();
-			if(scrollpos<height){
+			if(scrollpos<400){
 				var d = (scrollpos/(4*(400)))+0.75;
 			}
 			else {
 				var d = 1;
 			}
 		  document.getElementsByTagName('header')[0].style.background = "rgba(255,255,255,"+d+")";
-		  document.getElementById('navmenu').style.background = "rgba(255,255,255,"+d+")";
 		}
 		$(document).scroll(function(){fadeheader();});
 		$(document).ready(function(){fadeheader();});
